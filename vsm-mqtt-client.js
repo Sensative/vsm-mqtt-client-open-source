a701d0a1331c9dbbd51c65559306501f94f96472
Lars Mats <lars.mats@sensative.com>
2023-11-27 21:36:54 +0100

    Allow for async object store

--- vsm-mqtt-client.js
+++ vsm-mqtt-client.js
@@ -61,191 +61,194 @@ const printUsageAndExit = (hint) => {
   process.exit(1);
 }
 
-// Command line options handler (simple one)
-const args = require('minimist')(process.argv.slice(2));
-if (!args.i)
-  printUsageAndExit("-i <Integration name> is required");
-if (!(args.f || args.w))
-  printUsageAndExit("-f <device file> or -w is required");
+const run = async () => {
+  // Command line options handler (simple one)
+  const args = require('minimist')(process.argv.slice(2));
+  if (!args.i)
+    printUsageAndExit("-i <Integration name> is required");
+  if (!(args.f || args.w))
+    printUsageAndExit("-f <device file> or -w is required");
 
-args.v && console.log("Selected integration: " + args.i);
-args.v && console.log("Device file: " + args.f);
-args.v && console.log("Selected decorator: " + (args.d ? args.d : "default"));
+  args.v && console.log("Selected integration: " + args.i);
+  args.v && console.log("Device file: " + args.f);
+  args.v && console.log("Selected decorator: " + (args.d ? args.d : "default"));
 
-// Initialize and or connect to the storage
-initializeStore();
+  // Initialize and or connect to the storage
+  initializeStore();
 
-//
-// Create the list of devices (device ID dependent on each integration), as an array of files
-//
-let devices;
-if (args.f) {
-  devices = readDeviceList(args.f);
-  if (devices.length == 0) {
-    console.log("No devices in device file");
+  //
+  // Create the list of devices (device ID dependent on each integration), as an array of files
+  //
+  let devices;
+  if (args.f) {
+    devices = await readDeviceList(args.f);
+    if (devices.length == 0) {
+      console.log("No devices in device file");
+      printUsageAndExit();
+    }
+  } else if (!args.w)
+    printUsageAndExit();
+
+  //
+  // INTEGRATION
+  //
+  let integration = undefined;
+  try {
+    const location = process.env.VMC_INTEGRATIONS ? process.env.VMC_INTEGRATIONS : "./integrations";
+    integration = require(location + "/" + args.i);
+    if (!(integration.api && integration.api.getVersionString && integration.api.checkArgumentsOrExit && integration.api.connectAndSubscribe)) {
+      console.log("Integration " + args.i + " lacks a required function");
+      process.exit(1);
+    }
+  } catch (e) {
+    console.log(e.message);
     printUsageAndExit();
   }
-} else if (!args.w)
-  printUsageAndExit();
+  // Allow the integration to check its arguments (e.g. server, credentials, etc)
+  console.log("Integration: " + integration.api.getVersionString());
+  integration.api.checkArgumentsOrExit(args);
 
-//
-// INTEGRATION
-//
-let integration = undefined;
-try {
-  const location = process.env.VMC_INTEGRATIONS ? process.env.VMC_INTEGRATIONS : "./integrations";
-  integration = require(location + "/" + args.i);
-  if (!(integration.api && integration.api.getVersionString && integration.api.checkArgumentsOrExit && integration.api.connectAndSubscribe)) {
-    console.log("Integration " + args.i + " lacks a required function");
-    process.exit(1);
-  }
-} catch (e) {
-  console.log(e.message);
-  printUsageAndExit();
-}
-// Allow the integration to check its arguments (e.g. server, credentials, etc)
-console.log("Integration: " + integration.api.getVersionString());
-integration.api.checkArgumentsOrExit(args);
-
-//
-// DECORATION
-//
-let decorator;
-try {
-  const location = process.env.VMC_DECORATORS ? process.env.VMC_DECORATORS : "./decorators";
-  decorator = require(location + "/" + (args.d ? args.d : "default"));
-  if (!(decorator.api && decorator.api.decorate && decorator.api.getVersionString)) {
-    console.log("Decoration " + args.d + " lacks a required function");
-    process.exit(1);
-  }
-} catch (e) {
-  console.log(e.message);
-  printUsageAndExit();
-}
-console.log("Decoration: " + decorator.api.getVersionString());
-
-//
-// PUBLISHER
-// 
-
-let publisher = undefined;
-try {
-  const location = process.env.VMC_PUBLISHERS ? process.env.VMC_PUBLISHERS : "./publishers";
-  publisher = require(location + "/" + (args.O ? args.O : "console"));
-  if (!(publisher.api && publisher.api.getVersionString && publisher.api.checkArgumentsOrExit && publisher.api.publish)) {
-    console.log("Publisher " + args.O + " lacks a required function");
-    process.exit(1);
-  }
-} catch (e) {
-  console.log(e.message); 
-  printUsageAndExit();
-}
-// Allow the integration to check its arguments (e.g. server, credentials, etc)
-console.log("Publisher: " + publisher.api.getVersionString());
-publisher.api.checkArgumentsOrExit(args);
-publisher.api.initialize(args);
-
-//
-// SOLVER
-// 
-
-let solver = undefined;
-try {
-  const location = process.env.VMC_SOLVERS ? process.env.VMC_SOLVERS : "./solvers";
-  solver = require(location + "/" + (args.z ? args.z : "loracloud"));
-  if (!(solver.api && solver.api.getVersionString && solver.api.checkArgumentsOrExit && solver.api.solvePosition && solver.api.loadAlmanac && solver.api.initialize)) {
-    console.log("Solver " + args.z + " lacks a required function");
-    process.exit(1);
-  }
-} catch (e) {
-  console.log(e.message); 
-  printUsageAndExit();
-}
-// Allow the integration to check its arguments (e.g. server, credentials, etc)
-console.log("Positioning Solver: " + solver.api.getVersionString());
-solver.api.checkArgumentsOrExit(args);
-solver.api.initialize(args);
-
-// Function to handle uplinks for a device id on a port with binary data in buffer
-const onUplinkDevicePortBufferDateLatLng = async (client, deviceid, port, buffer, date, lat, lng, maxSize) => {
-  if (!(typeof(deviceid) == "string" && isFinite(port) && Buffer.isBuffer(buffer))) {
-    console.log(`Integration error: Bad parameter to onUplinkDevicePortBufferDateLatLng:
-                 typeof(deviceid):${typeof(deviceid)} (expect string), typeof(port)=${typeof(port)} (expect number), Buffer.isBuffer(buffer)=${Buffer.isBuffer(buffer)}`);
-    throw new Error("Bad parameter");
-  }
-
-  if (!isValidDate(date))
-    date = new Date();
-
-  // If wildcarded, check that we have the correct series of deveuis
-  if (args.w && !isVsmDevice(deviceid)) {
-    args.v && console.log("Ignoring unrecognized device " + deviceid);
-    return;
-  }
-
-  console.log("Uplink: device=" + deviceid + " port="+port + " buffer=" + buffer.toString("hex") + " date="+ date.toISOString() + " lat="+lat + " lng="+lng);
-
-  // Read previous state for this node
-  let previous = {};
+  //
+  // DECORATION
+  //
+  let decorator;
   try {
-    previous = fetchObjectFromStore(deviceid);
-    // args.v && console.log("previous:", previous);
-  } catch (e) {
-    args.v && console.log(`Note: No previous data for device ${deviceid}`);
-  }
-
-  // Run translation
-  let iotnode = { ...previous, 
-    encodedData : {
-        port : port,
-        hexEncoded : buffer.toString('hex'),
-        timestamp: date,  // TBD if this should be given by the integration instead?
-        maxSize: maxSize,
+    const location = process.env.VMC_DECORATORS ? process.env.VMC_DECORATORS : "./decorators";
+    decorator = require(location + "/" + (args.d ? args.d : "default"));
+    if (!(decorator.api && decorator.api.decorate && decorator.api.getVersionString)) {
+      console.log("Decoration " + args.d + " lacks a required function");
+      process.exit(1);
     }
-  }
-  let result = {}
-  try {
-    const returned = translator.translate(iotnode);
-    result = returned.result;
-    let timeseries = returned.timeseries;
-    // For now since there is no underlying timeseries database, ignore the timeseries part of the result, 
-    // e.g. data older than the most recent data.
-    args.v && timeseries && console.log("Ignoring historical timeseries data:", JSON.stringify(timeseries));
   } catch (e) {
-    console.log("Failed translation: ", e.message);
-    putErrorInStore(deviceid, e);
+    console.log(e.message);
+    printUsageAndExit();
   }
+  console.log("Decoration: " + decorator.api.getVersionString());
 
-  if (!result) {
-    // In case we are just filling up with time series data from previous measurements
-    console.log("No new results from translator\n");
-    return;
-  }
+  //
+  // PUBLISHER
+  // 
 
-  let next = mergeDeep(iotnode, result);
-
-  next = await processRules(args, integration, client, solver, deviceid, next, result, date, lat, lng);
-
-  // Store the next version of the object representation
-  putObjectInStore(deviceid, next);
-
-  // Publish the data
-  publisher.api.publish(args, deviceid, decorator.api.decorate(next));
-}
-
-const run = async () => {
-  // Let the integration create connection and add required subscriptions
-  let client = undefined;
+  let publisher = undefined;
   try {
-    client = await integration.api.connectAndSubscribe(args, devices, onUplinkDevicePortBufferDateLatLng);
+    const location = process.env.VMC_PUBLISHERS ? process.env.VMC_PUBLISHERS : "./publishers";
+    publisher = require(location + "/" + (args.O ? args.O : "console"));
+    if (!(publisher.api && publisher.api.getVersionString && publisher.api.checkArgumentsOrExit && publisher.api.publish)) {
+      console.log("Publisher " + args.O + " lacks a required function");
+      process.exit(1);
+    }
   } catch (e) {
-    console.log("Failed to connect and subscribe: " + e.message);
-    process.exit(1);
+    console.log(e.message); 
+    printUsageAndExit();
   }
-  while (true) {
-    args.v && console.log(new Date().toISOString() + " active");
-    await delay(60000);
-  }  
+  // Allow the integration to check its arguments (e.g. server, credentials, etc)
+  console.log("Publisher: " + publisher.api.getVersionString());
+  publisher.api.checkArgumentsOrExit(args);
+  publisher.api.initialize(args);
+
+  //
+  // SOLVER
+  // 
+
+  let solver = undefined;
+  try {
+    const location = process.env.VMC_SOLVERS ? process.env.VMC_SOLVERS : "./solvers";
+    solver = require(location + "/" + (args.z ? args.z : "loracloud"));
+    if (!(solver.api && solver.api.getVersionString && solver.api.checkArgumentsOrExit && solver.api.solvePosition && solver.api.loadAlmanac && solver.api.initialize)) {
+      console.log("Solver " + args.z + " lacks a required function");
+      process.exit(1);
+    }
+  } catch (e) {
+    console.log(e.message); 
+    printUsageAndExit();
+  }
+  // Allow the integration to check its arguments (e.g. server, credentials, etc)
+  console.log("Positioning Solver: " + solver.api.getVersionString());
+  solver.api.checkArgumentsOrExit(args);
+  solver.api.initialize(args);
+
+  // Function to handle uplinks for a device id on a port with binary data in buffer
+  const onUplinkDevicePortBufferDateLatLng = async (client, deviceid, port, buffer, date, lat, lng, maxSize) => {
+    if (!(typeof(deviceid) == "string" && isFinite(port) && Buffer.isBuffer(buffer))) {
+      console.log(`Integration error: Bad parameter to onUplinkDevicePortBufferDateLatLng:
+                  typeof(deviceid):${typeof(deviceid)} (expect string), typeof(port)=${typeof(port)} (expect number), Buffer.isBuffer(buffer)=${Buffer.isBuffer(buffer)}`);
+      throw new Error("Bad parameter");
+    }
+
+    if (!isValidDate(date))
+      date = new Date();
+
+    // If wildcarded, check that we have the correct series of deveuis
+    if (args.w && !isVsmDevice(deviceid)) {
+      args.v && console.log("Ignoring unrecognized device " + deviceid);
+      return;
+    }
+
+    console.log("Uplink: device=" + deviceid + " port="+port + " buffer=" + buffer.toString("hex") + " date="+ date.toISOString() + " lat="+lat + " lng="+lng);
+
+    // Read previous state for this node
+    let previous = {};
+    try {
+      previous = await fetchObjectFromStore(deviceid);
+      // args.v && console.log("previous:", previous);
+    } catch (e) {
+      args.v && console.log(`Note: No previous data for device ${deviceid}`);
+    }
+
+    // Run translation
+    let iotnode = { ...previous, 
+      encodedData : {
+          port : port,
+          hexEncoded : buffer.toString('hex'),
+          timestamp: date,  // TBD if this should be given by the integration instead?
+          maxSize: maxSize,
+      }
+    }
+    let result = {}
+    try {
+      const returned = translator.translate(iotnode);
+      result = returned.result;
+      let timeseries = returned.timeseries;
+      // For now since there is no underlying timeseries database, ignore the timeseries part of the result, 
+      // e.g. data older than the most recent data.
+      args.v && timeseries && console.log("Ignoring historical timeseries data:", JSON.stringify(timeseries));
+    } catch (e) {
+      console.log("Failed translation: ", e.message);
+      putErrorInStore(deviceid, e);
+    }
+
+    if (!result) {
+      // In case we are just filling up with time series data from previous measurements
+      console.log("No new results from translator\n");
+      return;
+    }
+
+    let next = mergeDeep(iotnode, result);
+
+    next = await processRules(args, integration, client, solver, deviceid, next, result, date, lat, lng);
+
+    // Store the next version of the object representation
+    await putObjectInStore(deviceid, next);
+
+    // Publish the data
+    publisher.api.publish(args, deviceid, decorator.api.decorate(next));
+  }
+
+  const runClient = async () => {
+    // Let the integration create connection and add required subscriptions
+    let client = undefined;
+    try {
+      client = await integration.api.connectAndSubscribe(args, devices, onUplinkDevicePortBufferDateLatLng);
+    } catch (e) {
+      console.log("Failed to connect and subscribe: " + e.message);
+      process.exit(1);
+    }
+    while (true) {
+      args.v && console.log(new Date().toISOString() + " active");
+      await delay(60000);
+    }  
+  }
+  await runClient();
 }
 
 run();
